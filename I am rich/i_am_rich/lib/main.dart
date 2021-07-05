7aa5b37e00a904b588100f4e8ccbc258e1b5610d
Achref ZNAIDIA <achref.znaidia@gmail.com>
2021-07-05 20:00:40 +0200

    rich

--- I am rich/i_am_rich/lib/main.dart
+++ I am rich/i_am_rich/lib/main.dart
@@ -8,6 +8,7 @@ class Logstate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
@@ -20,45 +21,78 @@ class Logstate extends StatelessWidget {
               children: <Widget>[
                 Container(
                   margin: EdgeInsets.all(10),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundColor: Colors.amber,
+                    foregroundImage: AssetImage('lib/Assets/avatar.png'),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.all(10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.account_circle_outlined,
+                        color: Colors.blueGrey[900],
+                      ),
+                      title: Text(
+                        'Achref ZNAIDIA',
+                        style: TextStyle(
+                          color: Colors.blueGrey[900],
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'PTSans',
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.blueGrey[900],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
                   child: Text(
                     'Devops Tools',
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
+                      fontFamily: 'PTSans',
                     ),
                   ),
                 ),
-                Image(
-                  image: AssetImage('lib/Assets/DevopsTools.jpg'),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Image(
+                    image: AssetImage('lib/Assets/DevopsTools.jpg'),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.blueGrey[900],
+                  ),
                 ),
                 Container(
                   height: 40,
                   width: 200,
-                  padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
+                  padding: EdgeInsets.fromLTRB(60, 10, 60, 10),
                   margin: EdgeInsets.all(10),
-                  color: Colors.blue,
-                  child: Text('achref'),
-                ),
-                Container(
-                  height: 40,
-                  width: 200,
-                  padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
-                  margin: EdgeInsets.all(10),
-                  color: Colors.green,
-                  child: Text('aya'),
-                ),
-                Container(
-                  height: 40,
-                  width: 200,
-                  padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
-                  margin: EdgeInsets.all(10),
-                  color: Colors.red,
-                  child: Text('Hama'),
-                ),
-                CircleAvatar(
-                  radius: 50,
-                  backgroundColor: Colors.amber,
-                  foregroundImage: AssetImage('lib/Assets/avatar.png'),
+                  color: Colors.blueGrey[900],
+                  child: Text(
+                    'Good',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'PTSans',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ]),
         ),
