e895d91898b08f8e30a8f82c9d27b70cf8454666
Achref ZNAIDIA <achref.znaidia@gmail.com>
2021-07-05 12:20:27 +0200

    rich

---
+++ I am rich/i_am_rich/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Logstate());
+}
+
+class Logstate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Text(
+                    'Devops Tools',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Image(
+                  image: AssetImage('lib/Assets/DevopsTools.jpg'),
+                ),
+                Container(
+                  height: 40,
+                  width: 200,
+                  padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
+                  margin: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  child: Text('achref'),
+                ),
+                Container(
+                  height: 40,
+                  width: 200,
+                  padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
+                  margin: EdgeInsets.all(20),
+                  color: Colors.green,
+                  child: Text('achref'),
+                ),
+                Container(
+                  height: 40,
+                  width: 200,
+                  padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
+                  margin: EdgeInsets.all(20),
+                  color: Colors.red,
+                  child: Text('achref'),
+                ),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.amber,
+                  foregroundImage: AssetImage('lib/Assets/avatar.png'),
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
