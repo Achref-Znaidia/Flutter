9c2151b3c87f4983c0aa1e1ab7880abcdd57c521
Achref ZNAIDIA <achref.znaidia@gmail.com>
2021-07-12 10:46:53 +0200

    mar

--- Market Manager/market_manager/lib/main.dart
+++ Market Manager/market_manager/lib/main.dart
@@ -14,10 +14,25 @@ class Logpage extends StatefulWidget {
 }
 
 class _LogpageState extends State<Logpage> {
+  Color _red = Color.fromARGB(255, 255, 56, 83);
+  Color _redclaire = Color.fromARGB(255, 255, 184, 197);
+  Color _amber = Colors.amber;
+  Color _white = Colors.white;
+  Color _black = Colors.black;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.red[400],
+        scaffoldBackgroundColor: Colors.red[400],
+        accentColor: Colors.amber,
+        textTheme: TextTheme(
+          bodyText1: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Colors.red[400],
         body: SafeArea(
@@ -26,7 +41,7 @@ class _LogpageState extends State<Logpage> {
               child: Column(
                 children: [
                   Expanded(
-                    flex: 8,
+                    flex: 7,
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -34,9 +49,9 @@ class _LogpageState extends State<Logpage> {
                           padding: const EdgeInsets.all(2.0),
                           child: SizedBox(
                             child: Text(
-                              'Sign in',
+                              'Sell your staff.',
                               style: TextStyle(
-                                color: Colors.black,
+                                color: _white,
                                 fontWeight: FontWeight.bold,
                                 fontSize: 14.0,
                                 //fontFamily: 'Arial Bold',
@@ -48,9 +63,10 @@ class _LogpageState extends State<Logpage> {
                           padding: const EdgeInsets.all(2.0),
                           child: SizedBox(
                             child: Text(
-                              'Sign in to your account',
+                              'Create a free account and get your market space.',
+                              textAlign: TextAlign.center,
                               style: TextStyle(
-                                color: Colors.black,
+                                color: _redclaire,
                                 fontWeight: FontWeight.bold,
                                 fontSize: 12.0,
                                 //fontFamily: 'Arial Bold',
@@ -68,7 +84,8 @@ class _LogpageState extends State<Logpage> {
                       children: [
                         Padding(
                           padding: const EdgeInsets.all(2.0),
-                          child: SizedBox(
+                          child: TextButton(
+                            onPressed: () {},
                             child: Text(
                               'Skip',
                               style: TextStyle(
@@ -84,47 +101,71 @@ class _LogpageState extends State<Logpage> {
                     ),
                   ),
                   Expanded(
-                    flex: 1,
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(10.0, 2.0, 10.0, 2.0),
-                      child: Card(
-                        color: Colors.red[50],
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Expanded(
-                              child: TextButton(
-                                style: TextButton.styleFrom(
-                                  backgroundColor: Colors.red[50],
+                    flex: 2,
+                    child: Container(
+                      width: 230,
+                      height: 60,
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            bottom: 8,
+                            left: 8,
+                            child: Container(
+                              width: 125,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: _white,
+                                border: Border.all(
+                                  color: _white,
                                 ),
-                                onPressed: () {},
-                                child: Text(
-                                  'Sign in',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                  ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
                                 ),
                               ),
-                            ),
-                            Expanded(
                               child: TextButton(
-                                style: TextButton.styleFrom(
-                                  backgroundColor: Colors.amber,
-                                  shape: const BeveledRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(5))),
-                                ),
                                 onPressed: () {},
                                 child: Text(
                                   'Register',
                                   style: TextStyle(
-                                    color: Colors.black,
+                                    color: _black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14.0,
+                                    //fontFamily: 'Arial Bold',
                                   ),
                                 ),
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                          Positioned(
+                            bottom: 8,
+                            right: 8,
+                            child: Container(
+                              width: 120,
+                              height: 60,
+                              decoration: BoxDecoration(
+                                color: _amber,
+                                border: Border.all(
+                                  color: _amber,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                              ),
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Sign in',
+                                  style: TextStyle(
+                                    color: _black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14.0,
+                                    //fontFamily: 'Arial Bold',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
