bd61738f133b4de56eb9753b7f3824e9011d2e71
Achref ZNAIDIA <achref.znaidia@gmail.com>
2021-07-17 09:10:07 +0200

    mar

--- Market Manager/market_manager/lib/constants.dart
+++ Market Manager/market_manager/lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-//const Color red = Color.fromARGB(255, 255, 56, 83);
-//const Color redclaire = Color.fromARGB(255, 255, 184, 197);
+const Color red = Color.fromARGB(255, 255, 56, 83);
+const Color redclaire = Color.fromARGB(255, 255, 184, 197);
 const Color amber = Colors.amber;
 const Color white = Colors.white;
 const Color black = Colors.black;
