a68953ee3583d341b30b6315a221559a7445370a
Achref ZNAIDIA <achref.znaidia@gmail.com>
2021-07-17 09:10:31 +0200

    mar

--- Market Manager/market_manager/lib/signinpage.dart
+++ Market Manager/market_manager/lib/signinpage.dart
@@ -11,6 +11,15 @@ class Signin extends StatefulWidget {
 
 class _SigninState extends State<Signin> {
   final usermail = TextEditingController();
+  final password = TextEditingController();
+  bool _passwordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,27 +60,92 @@ class _SigninState extends State<Signin> {
             Expanded(child: Column()),
             Expanded(
               child: Container(
-                height: 20,
+                height: 10,
+                padding: EdgeInsets.all(10),
                 margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                  color: Colors.white,
-                ),
-                child: TextFormField(
-                  controller: usermail,
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: redclaire,
+                  border: Border.all(
+                    color: Colors.black,
                   ),
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.amber,
+                ),
+                child: Center(
+                  child: TextFormField(
+                    controller: usermail,
+                    decoration: InputDecoration(
+                      // filled: true,
+                      // fillColor: redclaire,
+                      // border: OutlineInputBorder(
+                      //     borderRadius: BorderRadius.all(Radius.circular(20))),
+                      hintText: 'Email',
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      icon: Icon(Icons.mail_rounded),
+                    ),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: black,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: 10,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  color: redclaire,
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                ),
+                child: Center(
+                  child: TextFormField(
+                    controller: password,
+                    obscureText: _passwordVisible,
+                    decoration: InputDecoration(
+                      // filled: true,
+                      // fillColor: redclaire,
+                      // border: OutlineInputBorder(
+                      //     borderRadius: BorderRadius.all(Radius.circular(20))),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      icon: Icon(Icons.lock_rounded),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          // Based on passwordVisible state choose the icon
+                          _passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: Theme.of(context).primaryColorDark,
+                        ),
+                        onPressed: () {
+                          // Update the state i.e. toogle the state of passwordVisible variable
+                          setState(() {
+                            _passwordVisible = !_passwordVisible;
+                          });
+                        },
+                      ),
+                    ),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: black,
+                    ),
                   ),
                 ),
               ),
@@ -91,35 +165,40 @@ class _SigninState extends State<Signin> {
               ),
             ),
             Expanded(
+              flex: 2,
               child: Positioned(
                 bottom: 8,
                 right: 8,
-                child: Container(
-                  width: 120,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: amber,
-                    border: Border.all(
+                left: 8,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 230,
+                    height: 60,
+                    decoration: BoxDecoration(
                       color: amber,
+                      border: Border.all(
+                        color: amber,
+                      ),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
                     ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20),
-                    ),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      // Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(builder: (context) => Signin()),
-                      // );
-                    },
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(
-                        color: black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14.0,
-                        //fontFamily: 'Arial Bold',
+                    child: TextButton(
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(builder: (context) => Signin()),
+                        // );
+                      },
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          color: black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14.0,
+                          //fontFamily: 'Arial Bold',
+                        ),
                       ),
                     ),
                   ),
